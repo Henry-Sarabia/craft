47b2f626240f6bf41a98e89c66910524c00a15e8
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2018-10-09 16:14:19 -0700

    Add item template loading and tests

--- itemtemplate.go
+++ itemtemplate.go
@@ -1,15 +1,46 @@
 package craft
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
 type itemTemplate struct {
-	Name             string    `json:"name"`
-	ItemClass        itemClass `json:"item_class"`
-	BaseValue        float64   `json:"base_value"`  // Measured in gold pieces
-	BaseWeight       float64   `json:"base_weight"` // Measured in pounds
-	Aliases          []string  `json:"aliases"`
-	MaterialVariants []string  `json:"material_variants"`
-	DetailVariants   []string  `json:"detail_variants"`
+	Name             string   `json:"name"`
+	ItemClass        string   `json:"item_class"`
+	BaseValue        float64  `json:"base_value"`  // Measured in gold pieces
+	BaseWeight       float64  `json:"base_weight"` // Measured in pounds
+	Aliases          []string `json:"aliases"`
+	MaterialVariants []string `json:"material_variants"`
+	DetailVariants   []string `json:"detail_variants"`
 }
 
-func NewItem(temp itemTemplate, pool *ResourcePool) (*Item, error) {
-	return nil, nil
+// func NewItem(temp itemTemplate, pool *ResourcePool) (*Item, error) {
+// 	return nil, nil
+// }
+
+func readItemTemplates(r io.Reader) (map[string]itemTemplate, error) {
+	var it []itemTemplate
+
+	if err := json.NewDecoder(r).Decode(&it); err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]itemTemplate)
+	for _, v := range it {
+		m[v.Name] = v
+	}
+
+	return m, nil
+}
+
+func loadItemTemplates(filename string) (map[string]itemTemplate, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return readItemTemplates(f)
 }
---
+++ itemtemplate_test.go
@@ -0,0 +1,68 @@
+package craft
+
+import (
+	"os"
+	"testing"
+)
+
+const testItemTemplateFile = "testdata/itemtemplate_test.json"
+
+func TestReadItemTemplates(t *testing.T) {
+	f, err := os.Open(testItemTemplateFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	it, err := readItemTemplates(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(it) != 5 {
+		t.Errorf("got: <%v>, want: <%v>", len(it), 5)
+	}
+
+	if it["figurine"].Name != "figurine" {
+		t.Errorf("got: <%v>, want: <%v>", it["figurine"].Name, "figurine")
+	}
+
+	if len(it["doll"].MaterialVariants) != 2 {
+		t.Errorf("got: <%v>, want: <%v>", len(it["doll"].MaterialVariants), 2)
+	}
+}
+
+func TestReadItemTemplatesEmpty(t *testing.T) {
+	var f *os.File
+
+	_, err := readItemTemplates(f)
+	if err == nil {
+		t.Errorf("got: <%v>, want: <nil>", err)
+	}
+}
+
+func TestLoadItemTemplates(t *testing.T) {
+	it, err := loadItemTemplates(testItemTemplateFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(it) != 5 {
+		t.Errorf("got: <%v>, want: <%v>", len(it), 5)
+	}
+
+	if it["figurine"].Name != "figurine" {
+		t.Errorf("got: <%v>, want: <%v>", it["figurine"].Name, "figurine")
+	}
+
+	if len(it["doll"].MaterialVariants) != 2 {
+		t.Errorf("got: <%v>, want: <%v>", len(it["doll"].MaterialVariants), 2)
+	}
+}
+
+func TestLoadItemTemplatesEmpty(t *testing.T) {
+	_, err := loadItemTemplates("fake file name")
+	if err == nil {
+		t.Errorf("got: <%v>, want: <nil>", err)
+	}
+}
