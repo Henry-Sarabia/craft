1905e84c5a6384d4a42635efc3f07588ffdfc015
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2018-10-08 22:50:09 -0700

    Add detail loading and tests

--- detail.go
+++ detail.go
@@ -1,6 +1,37 @@
 package craft
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
 type detail struct {
 	Name     string   `json:"name"`
 	Variants []string `json:"variants"`
+}
+
+func readDetails(r io.Reader) (map[string]detail, error) {
+	var d []detail
+
+	if err := json.NewDecoder(r).Decode(&d); err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]detail)
+	for _, v := range d {
+		m[v.Name] = v
+	}
+
+	return m, nil
+}
+
+func loadDetails(filename string) (map[string]detail, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return readDetails(f)
 }
---
+++ detail_test.go
@@ -0,0 +1,68 @@
+package craft
+
+import (
+	"os"
+	"testing"
+)
+
+const testDetailFile = "testdata/detail_test.json"
+
+func TestReadDetails(t *testing.T) {
+	f, err := os.Open(testDetailFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	d, err := readDetails(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(d) != 4 {
+		t.Errorf("got: <%v>, want: <%v>", len(d), 4)
+	}
+
+	if d["beverage"].Name != "beverage" {
+		t.Errorf("got: <%v>, want: <%v>", d["beverage"].Name, "beverage")
+	}
+
+	if len(d["humanoid"].Variants) != 23 {
+		t.Errorf("got: <%v>, want: <%v>", len(d["humanoid"].Variants), 23)
+	}
+}
+
+func TestReadDetailsEmpty(t *testing.T) {
+	var f *os.File
+
+	_, err := readDetails(f)
+	if err == nil {
+		t.Errorf("got: <%v>, want: <nil>", err)
+	}
+}
+
+func TestLoadDetails(t *testing.T) {
+	d, err := loadDetails(testDetailFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(d) != 4 {
+		t.Errorf("got: <%v>, want: <%v>", len(d), 4)
+	}
+
+	if d["beverage"].Name != "beverage" {
+		t.Errorf("got: <%v>, want: <%v>", d["beverage"].Name, "beverage")
+	}
+
+	if len(d["humanoid"].Variants) != 23 {
+		t.Errorf("got: <%v>, want: <%v>", len(d["humanoid"].Variants), 23)
+	}
+}
+
+func TestLoadDetailsError(t *testing.T) {
+	_, err := loadDetails("fake file name")
+	if err == nil {
+		t.Errorf("got: <%v>, want: <nil>", err)
+	}
+}
