6553c8c055d474725f5162552f09dca1588a4abc
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2018-10-10 20:10:22 -0700

    Add randomString and tests

--- random.go
+++ random.go
@@ -1,1 +1,17 @@
 package craft
+
+import (
+	"errors"
+	"math/rand"
+)
+
+var errEmptySlice = errors.New("string slice is empty")
+
+func randomString(s []string) (string, error) {
+	if len(s) < 1 {
+		return "", errEmptySlice
+	}
+
+	r := rand.Intn(len(s))
+	return s[r], nil
+}
